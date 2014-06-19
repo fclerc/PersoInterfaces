823bc210c6130820ef6e27647897d9760f476f1f
fclerc <fc@florianclerc.com>
2014-06-19 15:45:07 +0200

    Scales translation and 'current' parameters bug correction

--- generateJSONFromXMLSchema.php
+++ generateJSONFromXMLSchema.php
@@ -1,10 +1,10 @@
 <?php
 
+$schemaFileName = 'context.xsd';
+$outputFile = 'contextScales.json';
 
-
-//$t = array('ivi' => array('a' => 'b'), 'ijbv' => 125);
 $xml = new DOMDocument();
-$xml->load('learnerMoocProfile.xsd');
+$xml->load($schemaFileName);
 $elements = $xml->getElementsByTagName('element');
 $xpath = new DOMXpath($xml);//used for more complex queries
 $dictionnary = array();
@@ -12,11 +12,10 @@ $dictionnary = array();
 foreach ($elements as $element) {
     $name = $element->getAttribute('name');
     if(!array_key_exists($name, $dictionnary)){//element not yet added
-        if($element->hasAttribute('type')){//we have to find the type somewhere else, or it is a pre-defined type
+        if($element->hasAttribute('type')){//we have to find the type definition somewhere else, or it is a pre-defined type
             $type = $element->getAttribute('type');
             //xpath expression to see if the type is complex and defined somewhere else
             $searchString = "count(//xs:complexType[@name='".$type."'])";
-            
             $complexTypeIsDefined = $xpath->evaluate($searchString);
             
             
@@ -102,23 +101,7 @@ function treatSimpleType($name, $simpleTypeElement){
 
 $json = json_encode($dictionnary);
 echo $json;
-file_put_contents('profileScales.json', $json);
-//$array = json_decode($json,TRUE);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+file_put_contents($outputFile, $json);
 
 ?>
 
