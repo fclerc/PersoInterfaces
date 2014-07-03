df2811b7848dcf5b51cc6a995ab28e7d4b6734ea
fclerc <fc@florianclerc.com>
2014-07-03 13:14:19 +0200

    Describing first part of the profile

--- presentation.php
+++ presentation.php
@@ -10,8 +10,9 @@
 	</head>
     
     <body>
-        <div class="container">
+        <div class="container presentation">
             <h1>Mise en place de la personnalisation dans le cadre des MOOCs</h1>
+            <p><a href="index.php" id="mainLink">Retour au menu principal</a></p>
             <ul>
                 <li><a href="#model">Présentation du modèle</a>
                 <li><a href="#appli">Présentation de l'application</a>
@@ -42,9 +43,34 @@
             <h3>Plus de détails sur...</h3>
             <p>Dans cette section, vous pourrez en apprendre plus sur chacun des éléments qui composent le modèle PERSUA2<sub>MOOC</sub> et son processus d'exploitation.</p>
             <h4>...les modèles utilisés</h4>
-            <p>Dans PERSUA2<sub>MOOC</sub>, des profils d'apprenant, contextes d'utilisation,...sont utilisés. A chacun de ces éléments correspond un modèle, que nous avons formalisé en XMLSchema. 3 niveaux de modèles,...</p>
+            <p>Dans PERSUA2<sub>MOOC</sub>, des profils d'apprenant, contextes d'utilisation,...sont utilisés. A chacun de ces éléments correspond un modèle, que nous avons formalisé en XMLSchema. Dans la mesure où chaque plateforme de MOOC est unique (les mêmes activités n'y sont pas disponibles, les informations collectées sur les apprenants et leurs actions peuvent différer), les profils d'apprenant ne vont pas être les mêmes sur chacune d'entre elles. De la même manière, pour deux MOOCs distincts hébergés sur la même plateforme, les informations exploitées sur les apprenants ne seront pas les mêmes, et dépendront avant tout des besoins exprimés par l'enseignant en vue de pouvoir opérer la personnalisation de la manière qui lui semble la plus efficace et pertinente.<br/>
+            En conséquence, il existe, pour chacun des modèles réalisés, trois niveaux (voici une explication détaillée pour le profil d'apprenant, il en va de même pour les autres) :</p>
+            <ul>
+                <li>Un modèle pour les MOOCs en général, qui donne l'organisation globale du profil (voir la partie suivante) et des exemples d'indicateurs, dont certains seront certainement systématiquement repris aux niveaux suivants. Il est bien évidemment fortement adaptable, afin de convenir à toutes les plateformes et MOOCs qui souhaiteraient l'exploiter.</li>
+                <li>Lorsqu'une plateforme de MOOC voudra mettre en place le processus de personnalisation, elle pourra ensuite modifier ce modèle d'apprenant qui lui est donné, pour l'adapter à sa situation propre. Dans la mesure où le modèle général, présenté dans le point précédent, a été réalisé en s'inspirant et en tenant fortement compte des plateformes actuelles de MOOC et des informations qu'elles collectent sur les apprenants, ce modèle adapté à une plateforme en particulier lui sera certainement très semblable.</li>
+                <li>Enfin, le modèle devra une nouvelle fois être adapté pour répondre aux besoins et à la configuration d'un MOOC en particulier. Cette phase a énormément d'importance, puisque c'est à ce stade qu'il va falloir tenir compte des ressources apportées par les enseignants et disponibles pour les apprenants. De nombreux indicateurs vont ainsi faire leur apparition au sein du profil comme par exemple 'Nombre de consultations de la vidéo de présentation générale', 'Résultat au quiz n°1',... Les enseignants vont de plus avoir la possibilité d'exprimer leurs besoins précis sur le MOOC, et dire quelles informations pertinentes ils veulent voir émerger des traces.</li>
+            </ul>
             <h4>...le profil d'apprenant</h4>
+            <p>Le profil d'apprenant comporte cinq sections, qui vont de la plus générale à propos de l'apprenant, à la plus précise concernant ses interactions avec les ressources qui lui sont proposées au sein du MOOC. Voici, sur un schéma les cunq sections et l'ordre dans lequel elles apparaissent (nous revenons ensuite sur chacune d'entre elles avec plus de précisions).</p>
+            <img src='img/learnerMoocProfile.PNG' alt='Structure profil apprenant dans les MOOCs'/>
+            <h5>Section 'learnerInformation'</h5>
+            <p>Cette première section contient des informations générales sur l'apprenant, qui ne sont pas extraites des traces, mais issues de questions qui peuvent être posées directement à l'apprenant (au moment de son inscription sur la plateforme ou lors de sa première connexion au MOOC). On y retrouve par exemple la date de naissance de l'apprenant, son sexe, sa situation professionnelle, son pays...(vous pourrez retrouver la liste complète des indicateurs qui sont pour l'instant présents au sein de cette section dans l'application elle-même).<p>
+            <h5>La section 'knowledge'</h5>
+            <p>Comme son nom l'indique, cette section va contenir des informations sur les connaissances et compétences de l'apprenant. Elle est subdivisée en deux sous-sections, la première concernant ses connaissances sur le sujet du MOOC (les indicateurs seront remplis en grande partie grâce à ses résultats aux quiz tout au long du MOOC). La deuxième a pour objet les outils qui sont utilisés dans le cadre du MOOC, comme par exemple, dans le cas de la programmation, la maîtrise d'un environnement de développement, ou dans d'autres matières la maîtrise d'outils comme une calculatrice. Dans la version générale du modèle de profil d'apprenant, tout ces indicateurs admettent des valeurs comprises entre 0 et 100 (100 signifiant que l'apprenant maîtrise totalement la connaissance ou compétence concernée).</p>
+            <h5>La section 'behaviour'</h5>
+            Dans cette section, ce sont les traces qui vont être exploitées de manière intensive pour obtenir des informations poussées sur l'apprenant,  des jugements qualitatifs concernant son comportement global sur la plateforme. Dans la version générale du modèle de profil, elle contient quelques indicateurs qui permettent de comprendre l'esprit et le type d'informations qui vont y être retrouvées. Un exemple d'indicateur que l'on y retrouve est 'studentPattern', issu de l'article  <a href="http://mfeldstein.com/combining-mooc-student-patterns-graphic-stanford-analysis/">Combining MOOC Student Patterns Graphic with Stanford Analysis</a>, par Phil Hill. Cet article indique, à partir de données tirées de MOOCs, que l'on peut segmenter les apprenants selon plusieurs catégories : </p>
+            <ul>
+                <li>les 'no-shows', qui ne se connectent jamais au MOOC après s'y être inscrits</li>,
+                <li>les 'active completing', qui réussissent le MOOC tout en y participant activement, sur le forum par exemple,</li>
+                <li>les 'passive completing' qui arrivent également à terminer le MOOC, mais sans trop participer au forum ni aux projets avancés,</li>
+                <li>les 'auditing', qui suivent le cours durant sa majeure partie, mais ne répondent pas ou peu aux questionnaires et examens qui leur sont proposés,</li>
+                <li>les 'disengaging' qui sont dans les catégories 'completing' au début du MOOC, mais vont progressivement moins fréquenter le MOOC,</li>
+                <li>les 'sampling' qui vont simplement consulter quelques ressources du MOOC, mais sans aller vraiment plus loin</li>
+            <p>Pour déterminer à quelle catégorie appartient un apprenant, les traces sont exploitées afin de savoir quelle quantité de ressources il consulte à chaque séquence, s'il se rend sur le forum ou non,...<br/>
+            Pour les autres indicateurs, vous pouvez vous reporter au profil utilisé dans l'application, et cliquer sur les bulles d'information pour lire la documentation concernant chacun des indicateurs.
             
+            Cette section pourra être très différente d'une plateforme de MOOC à l'autre, puisqu'elle dépend beacuoup des traces qui sont collectées, et surtout des traitements qui sont réalisés sur elles.
+            </p>
             <h4>...le contexte 'live'</h4>
             <h4>...le contexte de séquence</h4>
             <h4>...la stratégie pédagogique</h4>
